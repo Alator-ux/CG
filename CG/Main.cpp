6af1e3c5af8c75468be064013cd92f2f155fbe77
alator-ux <vavilkin.max@mail.ru>
2022-10-01 20:05:22 +0300

    linked imgui

--- CG/Main.cpp
+++ CG/Main.cpp
@@ -1,4 +1,4 @@
-#include <GL/glew.h>
+/*#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <SFML/OpenGL.hpp>
 #include <SFML/Window.hpp>
@@ -204,4 +204,52 @@ void Draw(OpenGLManager* manager) {
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glDisableVertexAttribArray(vPos);
     }
+}
+*/
+
+#include "imgui.h" // necessary for ImGui::*, imgui-SFML.h doesn't include imgui.h
+
+#include "imgui-SFML.h" // for ImGui::SFML::* functions and SFML-specific overloads
+
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(window, event);
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::ShowDemoWindow();
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
+        window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+
+    return 0;
 }
