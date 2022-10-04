6355e1656c80a6f87f0586201750aa787ce834f0
Sakorni <ssakorni@gmail.com>
2022-10-04 21:39:50 +0300

    First verision of affine algorythms

--- CG/PrimitiveChanger.h
+++ CG/PrimitiveChanger.h
@@ -1,14 +1,108 @@
 #pragma once
 #include "Primitives.h"
+#include "GLM/mat3x3.hpp"
+#include "GLM/vec3.hpp"
+#include "GLM/glm.hpp"
 #include <vector>
+#include <cmath>
+
+const double PI = 3.141592653589793238463;
+const int empty_item = -1;
 
 class PrimitiveChanger {
     std::vector<Primitive>* storage;
     int active_item;
     int mode;
+
+    // Converts provided angle to it's radian presentation
+    double toRadians(double angle) {
+        return PI * angle / 180.0;
+    }
+
+    // Returns matrix which needed for point rotation.
+    // x and y are the coordinates of point, which we are roating around
+    glm::mat3x3 build_rotation_matrix(double x, double y, double angle) {
+        auto rad_angle = this->toRadians(angle);
+        auto cos_r_a = cos(rad_angle);
+        auto sin_r_a = sin(rad_angle);
+        glm::mat3x3 shift_matrix1 = glm::mat3x3(
+            1, 0, 0,
+            0, 1, 0,
+            -x, -y, 1);
+        glm::mat3x3 shift_matrix2 = glm::mat3x3(
+            1, 0, 0,
+            0, 1, 0,
+            x, y, 1);
+        glm::mat3x3 rotation_matrix = glm::mat3x3(
+            cos_r_a, -sin_r_a, 0,
+            sin_r_a, cos_r_a, 0,
+            0, 0, 1);
+        return (shift_matrix1 * rotation_matrix * shift_matrix2);
+    }
+
+    glm::mat3x3 build_scale_matrix(double x, double y, double x_factor, double y_factor) {
+        glm::mat3x3 shift_matrix1 = glm::mat3x3(
+            1, 0, 0,
+            0, 1, 0,
+            -x, -y, 1);
+        glm::mat3x3 shift_matrix2 = glm::mat3x3(
+            1, 0, 0,
+            0, 1, 0,
+            x, y, 1);
+        glm::mat3x3 scaling_matrix = glm::mat3x3(
+            x_factor, 0, 0,
+            0, y_factor, 0,
+            0, 0, 1);
+        return (shift_matrix1 * scaling_matrix * shift_matrix2);
+    }
 public:
     PrimitiveChanger(std::vector<Primitive>* storage) {
         this->storage = storage;
         this->active_item = -1;
+    }
+
+    void shift(int item_index, int dx, int dy) {
+        if (item_index == empty_item) {
+            return;
+        }
+        
+        glm::mat3x3 shift_matrix = glm::mat3x3(1, 0, 0, 
+                                               0, 1, 0, 
+                                               dx, dy, 1);
+        // TODO: Максим, у меня ощущение, что здесь я копирую. Надо по ссылке тот элемент получать
+        auto item = (* storage)[item_index];
+        auto points = item.points;
+        for (int i = 0; i < points.size(); i++) {
+            glm::vec3 values = glm::vec3(points[i][0], points[i][1], 1);
+            auto shifted_point = values * shift_matrix;
+            //TODO: Если по ссылке - запарно, то просто тут нужному айтему присваивать
+            item.points[i] = shifted_point;
+        }
+    }
+    void rotate_around_point(int item_index, glm::vec3 point, double angle) {
+        if (item_index == empty_item) {
+            return;
+        }
+        glm::mat3x3 rotation_matrix = this->build_rotation_matrix(point[0], point[1], angle);
+        auto item = (*storage)[item_index];
+        auto points = item.points;
+        for (int i = 0; i < points.size(); i++) {
+            glm::vec3 values = glm::vec3(points[i][0], points[i][1], 1);
+            item.points[i] = values * rotation_matrix;
+        }
+    }
+
+    void scale_from_point(int item_index, glm::vec3 point, double x_factor, double y_factor) {
+        if (item_index == empty_item) {
+            return;
+        }
+        auto scale_matrix = this->build_scale_matrix(point[0], point[1], x_factor, y_factor);
+        auto item = (*storage)[item_index];
+        auto points = item.points;
+        for (int i = 0; i < points.size(); i++) {
+            glm::vec3 values = glm::vec3(points[i][0], points[i][1], 1);
+            //TODO: Если по ссылке - запарно, то просто тут нужному айтему присваивать
+            item.points[i] = values * scale_matrix;
+        }
     }
 };
